4e5be288ba00a5816232b34845af414c90b7125b
Brenton Jackson <bjavonjackson@gmail.com>
2022-03-20 12:49:09 -0400

    added twilio messaging endpoint

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-
 import priceRoutes from "./routes/price.js";
+import smsRoutes from "./routes/sms.js";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/price", priceRoutes);
+app.use("/sms", smsRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
